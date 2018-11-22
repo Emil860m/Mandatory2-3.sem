7e316d42c9aca392ce38483cf0fb725c633dd59e
Emil860m <fenger.emil@gmail.com>
2018-11-22 09:35:27 +0100

    Skeleton 1.0

---
+++ src/main/java/Course.java
@@ -0,0 +1,18 @@
+public class Course {
+    private String name_eng;
+    private String name_da;
+    private String study_programme;//maybe arraylist for more than 1
+    private boolean mandatory;
+    private int ECTS;
+    private String language;
+    private int min_students;
+    private int expected_students;
+    private int max_students;
+    private String prerequisites;
+    private String learning_outcome;
+    private String content;
+    private String learning_activities;
+    private String exam_form;
+    private String Teachers;//maybe arraylist for more than 1
+
+}

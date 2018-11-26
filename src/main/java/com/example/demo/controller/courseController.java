69da03fb18f526edda1a694805c34d9b18612cec
michjens <michael.diksi@hotmail.com>
2018-11-26 14:34:09 +0100

    getmapping til createCourse

--- src/main/java/com/example/demo/controller/courseController.java
+++ src/main/java/com/example/demo/controller/courseController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Course;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class courseController {
 
-    @GetMapping("/")
-    public String index(){
-        return "courses";
+    @GetMapping("/course/create")
+    public String createCourse(Model m){
+        m.addAttribute("course", new Course());
+        return "CourseNew";
     }
 }
